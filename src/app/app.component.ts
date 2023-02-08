ea0ec81c95db14677f3bd271df4dcc1ff761ea70
l.dutheil <l.dutheil@betclicgroup.com>
2023-02-08 16:38:31 +0100

    add apicall service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiCallService } from './services/api-call/api-call.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Firadenda';
+
+  private _ApiCall = inject(ApiCallService);
+
+  ngOnInit() {
+    this._ApiCall.getItems();
+  }
 }
