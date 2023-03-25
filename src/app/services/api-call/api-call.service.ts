fff84f4360d0cee579acdbc15ae41c84f9513e43
lucdu <luc.dutheil@gmail.com>
2023-03-25 03:04:06 +0100

    call api refacto

--- src/app/services/api-call/api-call.service.ts
+++ src/app/services/api-call/api-call.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root',
@@ -7,17 +8,31 @@ import { HttpClient } from "@angular/common/http";
 export class ApiCallService {
   constructor(private http: HttpClient ) {}
 
-  public get(url: string, options?: any) {
-    return this.http.get(url, options);
+  API_URL = 'https://api.example.com/';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  public getData(): Observable<any> {
+    return this.http.get(this.API_URL + 'data', this.httpOptions);
   }
-  public post(url: string, data: any, options?: any) {
-    return this.http.post(url, data, options);
+
+  public getIdData(id: number): Observable<any> {
+    return this.http.get(this.API_URL + `data/${id}`, this.httpOptions);
   }
-  public put(url: string, data: any, options?: any) {
-    return this.http.put(url, data, options);
+
+  public postData(data: any): Observable<any> {
+    return this.http.post(this.API_URL + 'data', data, this.httpOptions);
   }
-  public delete(url: string, options?: any) {
-    return this.http.delete(url, options);
+
+  public putData(id: number, data: any): Observable<any> {
+    return this.http.put(this.API_URL + `data/${id}`, data, this.httpOptions);
+  }
+
+  public deleteData(id: number): Observable<any> {
+    return this.http.delete(this.API_URL + `data/${id}`, this.httpOptions);
   }
 
 }
