747669b1d1cdf778e0f166f83992e9005bd2a8ff
lucdu <luc.dutheil@gmail.com>
2023-03-29 06:19:52 +0200

    pdf in progress

--- src/app/command/command.component.ts
+++ src/app/command/command.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiCallService } from '../services/api-call/api-call.service';
 import { Order } from '../interfaces/order.interface';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-command',
@@ -15,5 +16,28 @@ export class CommandComponent {
 
   ngOnInit(): void {
     this.commands$ = this.productService.getCommands();
+  }
+
+  downloadPdf() {
+    const doc = new jsPDF();
+    const commands = this.commands$;
+    let y = 10;
+
+    commands.subscribe((data) => {
+      data.forEach((command) => {
+        doc.text(`Order ${command.id}`, 10, y);
+        y += 10;
+        command.items.forEach((item) => {
+          doc.text(`${item.name} - ${item.price} - Item quantity mock`, 10, y);
+          y += 10;
+        });
+        doc.text(`Total: ${command.total}`, 10, y);
+        y += 10;
+        doc.text(`Address: ${command.address}`, 10, y);
+        y += 20;
+      });
+    });
+
+    doc.save('commands.pdf');
   }
 }
